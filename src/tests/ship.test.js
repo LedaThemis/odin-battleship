5b52345a6c508685ed2dd7e674f4ca70e42973de
Leda <ledathemis@protonmail.com>
2022-04-18 21:42:34 +0300

    call hit() twice to check for past hit values

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -1,8 +1,11 @@
 const Ship = require('../modules/ship');
 
-test('update hit array position after hit() call', () => {
+test('update hit array after hit() call', () => {
   const ship = Ship(3);
-  ship.hit(0);
 
+  ship.hit(0);
   expect(ship.getHitArray()).toContain(0);
+
+  ship.hit(2);
+  expect(ship.getHitArray()).toContain(2);
 });
