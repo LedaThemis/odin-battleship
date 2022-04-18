3ffb18618c92222b0f4753b47015625eac751729
Leda <ledathemis@protonmail.com>
2022-04-18 21:35:21 +0300

    add test for hit() functionality

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -1,1 +1,8 @@
-const ship = require('../modules/ship');
+const Ship = require('../modules/ship');
+
+test('update hit array position after hit() call', () => {
+  const ship = Ship(3);
+  ship.hit(0);
+
+  expect(ship.getHitArray()).toContain(0);
+});
