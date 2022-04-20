4bbc93d733ba2b2dcfb4086a28e7f6d982bd9643
Leda <ledathemis@protonmail.com>
2022-04-21 02:43:17 +0300

    add method to populate board container with board block divs

---
+++ src/utils/populateBoard.js
@@ -0,0 +1,24 @@
+const createBoardBlockDiv = (id, height, width) => {
+  const div = document.createElement('div');
+  div.dataset.key = id;
+  div.classList.add('board-block');
+
+  div.style.height = `${height}px`;
+  div.style.width = `${width}px`;
+
+  return div;
+};
+
+const computeBoardBlockDimensions = (boardSize, boardBlocksCount) => {
+  return { height: boardSize / Math.sqrt(boardBlocksCount), width: boardSize / Math.sqrt(boardBlocksCount) };
+};
+
+const populateBoard = (boardDiv, boardBlocksCount) => {
+  const { height, width } = computeBoardBlockDimensions(boardDiv.clientHeight, boardBlocksCount);
+  for (let i = 0; i < boardBlocksCount; i++) {
+    const div = createBoardBlockDiv(i, height, width);
+    boardDiv.appendChild(div);
+  }
+};
+
+export default populateBoard;
