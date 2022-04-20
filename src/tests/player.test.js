e6bad314ed24030f37e2b49d55060a0867c58c62
Leda <ledathemis@protonmail.com>
2022-04-21 00:38:09 +0300

    add tests for player.js

---
+++ src/tests/player.test.js
@@ -0,0 +1,13 @@
+const Gameboard = require('../modules/gameBoard');
+const Player = require('../modules/player');
+
+const attackMock = jest.fn((index) => index);
+
+test('check that player calls enemyGameBoard.attack() with given index', () => {
+  const enemyGameBoardMock = { attack: attackMock };
+  const player = Player('Name', enemyGameBoardMock);
+  player.play(2);
+
+  expect(enemyGameBoardMock.attack.mock.calls.length).toBe(1);
+  expect(enemyGameBoardMock.attack.mock.calls[0][0]).toBe(2); // first call, first argument
+});
