b6d674f3cd54d9971156304e5b848e73dcd8348a
Leda <ledathemis@protonmail.com>
2022-04-21 03:20:55 +0300

    implement getTurn(), setTurn()

--- src/modules/player.js
+++ src/modules/player.js
@@ -1,12 +1,19 @@
 const Player = (name, enemyGameBoard) => {
   let _name = name;
   let _playedPositions = [];
+  let _turn = false;
 
   const _enemyGameBoard = enemyGameBoard;
 
   const getName = () => {
     return _name;
   };
+
+  const setTurn = (newState) => {
+    _turn = !!newState; // convert non-boolean values
+  };
+
+  const getTurn = () => _turn;
 
   const play = (index) => {
     _playedPositions.push(index);
@@ -33,6 +40,8 @@ const Player = (name, enemyGameBoard) => {
     play,
     computerPlay,
     getName,
+    getTurn,
+    setTurn,
   };
 };
 
